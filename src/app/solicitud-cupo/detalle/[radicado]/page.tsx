74f5039a10275907e8c769c348dade6a436981c3
Artag/Chris <scristxyz@gmail.com>
2025-06-16 15:01:54 -0500

    act ruta

--- src/app/solicitud-cupo/detalle/[radicado]/page.tsx
+++ src/app/solicitud-cupo/detalle/[radicado]/page.tsx
@@ -1,9 +1,12 @@
+"use client"
+
 import SolicitudDetalle from "@/components/solicitud-detalle"
 
 interface PageProps {
   params: {
     radicado: string
   }
+  searchParams?: { [key: string]: string | string[] | undefined }
 }
 
 export default function DetalleSolicitudPage({ params }: PageProps) {
